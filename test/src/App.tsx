2f1df5b229e0e8dd9b5a2ecfdd95c04d08198995
celmartinez <cmartine@student.42.fr>
2020-08-10 10:33:59 +0200

    create-react-app with typescript

---
+++ test/src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "./logo.svg";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/*  <img src={logo} className="App-logo" alt="logo" /> */}
+                <a
+                    className="App-link"
+                    href="https://www.maiia.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Maiia
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
